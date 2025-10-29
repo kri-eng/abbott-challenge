9ae308e005901c7a6c5699a90b2d1f5c3213e417
Krish Patel <124129235+kri-eng@users.noreply.github.com>
2025-10-29 11:58:53 -0500

    Added State Management using Shared Preferences

--- lib/ui/heart/view_model/heart_view_model.dart
+++ lib/ui/heart/view_model/heart_view_model.dart
@@ -1,35 +1,60 @@
 import 'dart:async';
 import 'package:challenge/domain/models/heart_model.dart';
 import 'package:flutter/foundation.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HeartViewModel{
   
+  HeartViewModel() {
+    _loadState();
+  }
+
   final ValueNotifier<HeartModel> state = ValueNotifier<HeartModel>(const HeartModel(percentage: 0, timerRunning: false));
   Timer? _timer;
 
-  void startCounter() {
-    if (state.value.timerRunning) {
-      return;
-    } else {
-      state.value = state.value.copyWith(timerRunning: true);
-      _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-        if (state.value.percentage < 100) {
-          state.value = state.value.copyWith(percentage: state.value.percentage + 10);
-        } else {
-          pauseCounter();
-        }
-      });
+  Future<void> _saveState() async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('percentage', state.value.percentage);
+    await prefs.setBool('timerRunning', state.value.timerRunning);
+  }
+
+  Future<void> _loadState() async {
+    final prefs = await SharedPreferences.getInstance();
+    final percentage = prefs.getInt('percentage') ?? 0;
+    final timerRunning = prefs.getBool('timerRunning') ?? false;
+
+    state.value = HeartModel(percentage: percentage, timerRunning: timerRunning);
+
+    if (percentage < 100 && timerRunning) {
+      startCounter();
     }
+  }
+
+  void startCounter() {
+    _timer?.cancel();
+
+    state.value = state.value.copyWith(timerRunning: true);
+    _saveState();
+    _timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+      if (state.value.percentage < 100) {
+        state.value = state.value.copyWith(percentage: state.value.percentage + 10);
+        _saveState();
+      } else {
+        pauseCounter();
+      }
+    });
   }
 
   void pauseCounter() {
     _timer?.cancel();
     state.value = state.value.copyWith(timerRunning: false);
+    _saveState();
   }
 
   void resetCounter() {
     _timer?.cancel();
     state.value = const HeartModel(percentage: 0, timerRunning: false);
+    _saveState();
   }
 
   void pauseAndResumeCounter() {
